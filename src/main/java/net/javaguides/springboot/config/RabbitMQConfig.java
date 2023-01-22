1530f9bb8f506edf2963ed97f97200be4f9463ec
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-22 08:40:00 -0300

    Criando a configuração de conexão com RabbitMQ

---
+++ src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package net.javaguides.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key")
+    private String routingKey;
+
+    // spring bean for rabbitmq queue
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    // spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    //binding between and exchange using routing key
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+//https://www.youtube.com/watch?v=w21wvrnPYp0&list=PLGRDMO4rOGcMh2fAMOnwuBMDa8PxiKWoN&index=10
+}
+//docker run -d -p 15672:15672 -p 5672:5672  --hostname rabbitmq --name spring-rabbit -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=12345678 rabbitmq:3-management-alpine 
